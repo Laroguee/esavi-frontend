b22992c8b7f9489ed9d42a99ff54060fedef3fd9
Luis Alejandro Ramos Ordóñez <luis.ramos@srs.gob.sv>
2026-09-10 11:05:20 -0600

    chore: remove scratch files with sensitive data and ignore them

--- scratch_firestore.js
+++
@@ -1,6 +0,0 @@
-const admin = require('firebase-admin');
-// You can initialize the default app without credentials locally in the emulator
-// Or we can just read the firebase config from the project
-
-// It's probably easier to just write a simple script that imports the firebase db or we can check the react code.
-// Let's check `src/store/useAuthStore.ts` or somewhere else to see what users look like.
